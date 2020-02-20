3d0b14eb38d4a3885ae2fa63cf1a0ad1c1e03880
Nolan <nolan.schwartz@student.csulb.edu>
2020-02-19 18:05:29 -0800

    imperfect solution for PermMissingElem

--- Lesson 3 Time Complexity/PermMissingElem.js
+++ Lesson 3 Time Complexity/PermMissingElem.js
@@ -1,4 +1,6 @@
-const Array = [1, 2, 3, 5];
+const Array = [1, 2, 3, 1];
+const emptyArray = [];
+const singleArray = [1];
 
 /**
  * @return {number}
@@ -19,5 +21,45 @@ function PermMissingElem1(A) {
     return missingElem;
 }
 
+
+/**
+ * @return {number}
+ */
+function PermMissingElem2(A) {
+    let elementSet = new Set();
+
+    if (A.length === 0) {
+        return 1
+    }
+
+    if(A.length === 1 && A[0] === 1){
+        return 2
+    }
+
+
+    let index = A.length;
+
+    for (let i = 0; i < index; i++) {
+        if(index < A[i]){
+            index = A[i]
+        }
+
+        if (!elementSet.has(A[i])) {
+            elementSet.add(A[i])
+        }
+    }
+
+    for (let i = 1; i <= index; i++) {
+        if (!elementSet.has(i)) {
+            return i
+        }
+    }
+
+    return -1
+}
+
 // Should return 4
-console.log(PermMissingElem1(Array));
+// console.log(PermMissingElem1(Array));
+console.log(PermMissingElem2(Array));
+console.log(PermMissingElem2(emptyArray));
+console.log(PermMissingElem2(singleArray));
